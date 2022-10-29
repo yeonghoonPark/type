7d81e8b331f66982e8121179a9ce74e406decc07
yeonghoonPark <95198410+yeonghoonPark@users.noreply.github.com>
2022-10-29 20:06:03 +0900

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,20 @@
+// 참조사이트: https://joshua1988.github.io/ts/guide/basic-types.html
+
+/**
+ * 기본 타입
+ *  1. string
+ *  2. number
+ *  3. boolean
+ *  4. object
+ *  5. array
+ *  6. tuple
+ *  7. enum
+ *  8. any
+ *  9. void
+ *  10. null
+ *  11. undefined
+ *  12. never
+ */
+
+// 1. string
+let name: string = "yeonghoon";
